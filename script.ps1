45ce2818caa7553e243f794cc2f612e03977adf0
Manuel Galindo ⛰️ <magalin@microsoft.com>
2020-03-19 17:36:56 -0600

    added script

---
+++ script.ps1
@@ -0,0 +1,11 @@
+﻿$fileName = "Database1.mdb"
+$conn = New-Object System.Data.OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=$filename;Persist Security Info=False")
+$cmd=$conn.CreateCommand()
+$cmd.CommandText="Select * from customers"
+$conn.open()
+$rdr = $cmd.ExecuteReader()
+$dt = New-Object System.Data.Datatable
+$dt.Load($rdr)
+$dt
+
+
